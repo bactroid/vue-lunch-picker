c79d7f6eb18a78eb270255f58231cdce27da3dcc
Kate Haskell <khaskell@ufl.edu>
2018-10-19 08:53:48 -0400

    Initial working prototype

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,36 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <RestaurantDisplay :restaurant="choice" v-on:new-restaurant="getRestaurant" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios'
+import RestaurantDisplay from './components/RestaurantDisplay.vue'
 
 export default {
   name: 'app',
+  data: () => {
+    return { choice: 'Loading' }
+  },
+  methods: {
+    getRestaurant () {
+        this.choice = 'Loading'
+        axios.get('https://6zkojio7gj.execute-api.us-east-1.amazonaws.com/dev/lunch')
+          .then(x => {
+            this.choice = x.data.text
+          })
+          .catch(() => {
+            this.choice = 'ERROR'
+          })
+    }
+  },
+  created () {
+    this.getRestaurant()
+  },
   components: {
-    HelloWorld
+    RestaurantDisplay
   }
 }
 </script>
---
+++ src/components/RestaurantDisplay.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="hello">
+    <h1>What's for lunch today?</h1>
+    <p>{{ restaurant }}</p>
+    <button v-on:click="$emit('new-restaurant')">Nah. Pick something else.</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'RestaurantDisplay',
+  props: {
+    restaurant: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
